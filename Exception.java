2907d4b92a7dccb2d3c7b6cbc90966753c09ba85
igalna <igalna@gmail.com>
2014-11-17 17:53:39 +0000

    First Commit

---
+++ Exception.java
@@ -0,0 +1,12 @@
+try {
+    // more code here
+    
+    list.add(newElement);
+    
+    // more code here
+    } catch (NullPointerException ex) {
+        ex.printStackTrace();
+    } catch (Exception ex) {
+        ex.printStackTrace();
+    
+    }
